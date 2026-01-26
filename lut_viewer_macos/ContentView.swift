f8b13537a19a85f4e74308ec94bde6dbd4be82cf
doriup <ddodocamille@gmail.com>
2026-01-26 11:40:35 +0100

    Initial Commit

---
+++ lut_viewer_macos/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  lut_viewer_macos
+//
+//  Created by Dorian on 26/01/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ lut_viewer_macos/lut_viewer_macosApp.swift
@@ -0,0 +1,17 @@
+//
+//  lut_viewer_macosApp.swift
+//  lut_viewer_macos
+//
+//  Created by Dorian on 26/01/2026.
+//
+
+import SwiftUI
+
+@main
+struct lut_viewer_macosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
